01f7ea6cd7be730aeeb23dae7fdbad197cffee6f
Shon Feder <shon.feder@gmail.com>
2024-11-19 21:44:54 -0500

    Build image tag from flags
    As an alternative to trying to parse out variant data from an image tag,
    this approach exposes a more fine grained CLI that invites users to
    compose flags to describe the build environment they want.
    
    This effectively abstracts the details about the platform from the
    specific implementation in docker. This may also be useful if we try to
    support different backends instead of docker.
    
    E.g.
    
    opam-ci-check build lwt.5.9.0
    opam-ci-check build --distro ubuntu lwt.5.9.0
    opam-ci-check build --distro ubuntu --compiler 5.3  lwt.5.9.0
    opam-ci-check build --distro ubuntu --compiler 5.3  lwt.5.9.0
    opam-ci-check build --distro ubuntu --compiler 5.3 --hash sha256:af7ad8a0ca6fda6fe6e2429931f301412adb06e9db84e3c7f4ad3f49e475285e lwt.5.9.0

--- opam-ci-check/bin/main.ml
+++ opam-ci-check/bin/main.ml
@@ -71,79 +71,21 @@ let test_revdeps pkg local_repo_dir use_dune no_transitive_revdeps =
             (Printf.sprintf "tests failed in %d reverse dependencies"
                num_failed_installs)
 
-let default_compiler () =
-  let compiler =
-    Ocaml_version.(to_string (with_just_major_and_minor Releases.latest))
-  in
-  (compiler, None)
-
-let default_distro () =
-  (Distro.resolve_alias Distro.master_distro :> Distro.t)
-  |> Distro.tag_of_distro
-
-let parse_distro_compiler_hash base =
-  let img, hash =
-    match Astring.String.cut ~sep:"@" base with
-    | Some (s, hash) -> (s, Some hash)
-    | None -> (base, None)
-  in
-  let _docker_img, tag =
-    match Astring.String.cut ~sep:":" img with
-    | Some (docker_img, tag) -> (docker_img, tag)
-    | None -> ("ocaml/opam", img)
-  in
-  let distro, ocaml =
-    match Astring.String.cut ~sep:"-ocaml-" tag with
-    | Some (distro, ocaml) -> (Some distro, Some ocaml)
-    | None ->
-        if
-          String.starts_with ~prefix:"ocaml-" tag
-          || Str.string_match (Str.regexp "[0-9]") tag 0
-        then (None, Some tag)
-        else (Some tag, None)
-  in
-  let compiler =
-    let ( let* ) = Stdlib.Option.bind in
-    let* ocaml = ocaml in
-    let version_variant =
-      ocaml
-      |> Str.replace_first (Str.regexp "ocaml-") ""
-      |> Str.replace_first (Str.regexp {|-\(beta\|alpha\)|}) "~\\1"
-    in
-    let version, variant =
-      match Astring.String.cut ~sep:"-" version_variant with
-      | Some (version, variant) -> (version, Some variant)
-      | None -> (version_variant, None)
-    in
-    Some (version, variant)
-  in
-  (distro, compiler, hash)
-
-let config_from_base_image ~base_image ~with_tests ~lower_bounds pkg =
-  let distro, compiler, hash =
-    match base_image with
-    | Some base_image -> parse_distro_compiler_hash base_image
-    | None -> (None, None, None)
-  in
-  let opam_version = `Dev in
-  let distro = Stdlib.Option.value distro ~default:(default_distro ()) in
-  let compiler = Stdlib.Option.value compiler ~default:(default_compiler ()) in
-  (* TODO: Support other archs *)
-  let variant = Variant.v ~distro ~compiler ~arch:`X86_64 in
-  let base =
-    Spec.Docker
-      (if Option.is_some hash then Option.get base_image
-       else "ocaml/opam:" ^ Variant.docker_tag variant)
-  in
-  let config = Spec.opam ~variant ~lower_bounds ~with_tests ~opam_version pkg in
-  (base, config)
-
-let build_run_spec no_cache base_image with_tests lower_bounds pkg
-    opam_repository =
+let build_run_spec
+    ~variant
+    ~hash
+    ~no_cache
+    ~with_tests
+    ~lower_bounds
+    ~pkg
+    ~opam_repository =
   let pkg = OpamPackage.of_string pkg in
-  let base, config =
-    config_from_base_image ~base_image ~with_tests ~lower_bounds pkg
+  let base =
+    let hash = Stdlib.Option.(hash |> map (fun h -> "@" ^ h) |> value ~default:"") in
+    let image = Printf.sprintf "ocaml/opam:%s%s" (Variant.docker_tag variant) hash in
+    Spec.Docker image
   in
+  let config = Spec.opam ~variant ~lower_bounds ~with_tests ~opam_version:`Dev pkg in
   Test.build_run_spec ~use_cache:(not no_cache) ?opam_repository ~base config
   |> Result.map_error (fun _ -> "Failed to build and test the package")
 
@@ -275,24 +217,100 @@ let lower_bounds =
   in
   Arg.value (Arg.flag info)
 
-let base_image =
-  let info =
-    Arg.info [ "base-image" ]
-      ~doc:
-        "Base image to use for the build. Defaults to ocaml/opam on Debian \
-         stable with the latest OCaml release."
+let distro_conv =
+  let of_string s =
+    match Dockerfile_opam.Distro.distro_of_tag s with
+    | Some _ -> Ok s (* We are just validating that it is a supported distro *)
+    | None -> Error (`Msg (s ^ " is not a supported distro"))
   in
-  Arg.value (Arg.opt (Arg.some Arg.string) None info)
+  let pp = Fmt.string in
+  Arg.conv ~docv:"DISTRO" (of_string, pp)
+
+let arch_conv =
+  let of_string = Ocaml_version.arch_of_string in
+  let pp = Fmt.of_to_string Ocaml_version.string_of_arch in
+  Arg.conv ~docv:"ARCHITECTURE" (of_string, pp)
+
+let compiler_conv =
+  let of_string s =
+    Ocaml_version.of_string s
+    |> Result.map (fun v ->
+        let variant = Ocaml_version.extra v in
+        let version =
+          v
+          |> Ocaml_version.with_just_major_and_minor
+          |> Ocaml_version.to_string
+        in
+        (version, variant))
+  in
+  let pp =
+    Fmt.of_to_string @@
+    fun (ver, var) ->
+    let open Ocaml_version in
+    ver
+    |> of_string_exn
+    |> Fun.flip with_variant var
+    |> to_string
+  in
+  Arg.conv ~docv:"COMPILER" (of_string, pp)
+
+(* [(let+)] is [Term.(const f $ v)] *)
+let ( let+ ) t f = Term.(const f $ t)
+
+(* [(and+)] is [Term.(const (fun x y -> (x, y)) $ a $ b)] *)
+let ( and+ ) a b = Term.(const (fun x y -> (x, y)) $ a $ b)
+
+let variant =
+  let+ arch =
+    let info =
+      Arg.info [ "arch" ]
+        ~doc:"TODO"
+    in
+    Arg.value (Arg.opt arch_conv `X86_64 info)
+  and+ distro =
+    let default =
+      (Distro.resolve_alias Distro.master_distro :> Distro.t)
+      |> Distro.tag_of_distro
+    in
+    let info =
+      Arg.info [ "distro" ]
+        ~doc:"TODO"
+    in
+    Arg.value (Arg.opt distro_conv default info)
+  and+ compiler =
+    let default =
+      Ocaml_version.(to_string (with_just_major_and_minor Releases.latest)), None
+    in
+    let info =
+      Arg.info [ "compiler" ]
+        ~doc:"TODO"
+    in
+    Arg.value (Arg.opt compiler_conv default info)
+  in
+  Variant.v ~arch ~distro ~compiler
+
+let hash =
+  let info =
+    Arg.info [ "hash" ]
+      ~doc:"TODO"
+  in
+  Arg.value (Arg.(opt (some string) None) info)
 
 let build_cmd =
   let doc =
     "Build a package optionally with tests and/or using lower bounds packages"
   in
   let term =
-    Term.(
-      const build_run_spec $ no_cache $ base_image $ with_test $ lower_bounds
-      $ pkg_term $ local_opam_repo_term)
-    |> to_exit_code
+    to_exit_code @@
+    let+ variant 
+    and+ hash
+    and+ no_cache
+    and+ lower_bounds
+    and+ with_tests = with_test
+    and+ pkg = pkg_term
+    and+ opam_repository = local_opam_repo_term
+    in
+    build_run_spec ~variant ~hash ~no_cache ~with_tests ~lower_bounds ~pkg ~opam_repository
   in
   let info =
     Cmd.info "build" ~doc ~sdocs:"COMMON OPTIONS" ~exits:Cmd.Exit.defaults
